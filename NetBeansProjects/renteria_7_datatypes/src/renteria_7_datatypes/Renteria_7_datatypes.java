005fbc35df31b61cdb6498950e0fd598ebe6b867
irenteria <irenteria@ninja8.d22.lan>
2015-09-30 10:43:15 -0500

    datatypesisaack

---
+++ NetBeansProjects/renteria_7_datatypes/src/renteria_7_datatypes/Renteria_7_datatypes.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package renteria_7_datatypes;
+
+/**
+ *
+ * @author irenteria
+ */
+public class Renteria_7_datatypes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++)    
+       
+        
+        // TODO code application logic here
+        //space for comments
+        //code explanationis an expatation
+        //in d22
+        //one = sing is adingment 2 is comparement 
+       // int i = 9;
+       // float a = 2.5f;
+        //double b = 3.87;
+        //short c = 16;
+        //char d1 = ';';
+        //char d2 = 'a';
+        //char d3 = 42 ;
+        //char d4 = 145;
+        //String e = d4+"";
+        //String e1 = "im the best" ;
+        //boolean f = true ;
+        //boolean g = i  % 2 == 0;
+// + add - subtracts * multplies / divide % modulo shows remainder  
+        //System.out.println(i+ " an integer");
+        //System.out.println(a + "a float");
+        //System.out.println(b + "a double");
+        //System.out.println(c + "a short");
+        //System.out.println(d1 + "char 1");
+        //System.out.println(d2 + "char 2");
+        //System.out.println(d3 + "cahr 3");
+        //System.out.println(d4 + "char 4");
+        //System.out.println(e + "a string ");
+        //System.out.println(e1 + "sacond string ");
+        //System.out.println(f + "boolean") ;
+        //System.out.println(g + "another");
+       // for(i = -123;i<123; i++) {
+         //   System.out.println(i % 7 == 0);
+            // % checks for remainder i % 2 checks even and odd
+            // i % 3 evrey 3rd num true because multple by 3
+          // i = 7; 
+            //a = 0;
+           // e = "megatron";
+           // System.out.println(i + a + "an integer / " + "plus a float ");
+            //System.out.println(e + i + " a string puls an integer");
+            // up casting
+            // and an int and a sting combindns the int and string 
+         //   for (i=0; i <500; i++){
+                // if is conditional 
+           //     if (i % 2 ==0 ){
+                 //   System.out.println(i + " is even "); }
+             //   else{
+                
+               //     System.out.println(i + " is odd");}
+               // }
+       
+    
+    
+       
+        }
+    
+
+
