9deba2e693b50a4a94a241372c9500dc10d1db52
irenteria <irenteria@ninja8.d22.lan>
2015-11-20 16:12:05 -0600

    im not done just pushing

--- NetBeansProjects/renteria_7_textbasegame/src/renteria_7_textbasegame/Renteria_7_textbasegame.java
+++ NetBeansProjects/renteria_7_textbasegame/src/renteria_7_textbasegame/Renteria_7_textbasegame.java
@@ -37,42 +37,44 @@ public class Renteria_7_textbasegame {
         }
 
         while (!no & !end) {
-
+// you are the hero #hero
             System.out.println("you have woken up in a unknowwn city ");
             System.out.println("you pullout your phone and you can call your mom your frenid or look at maps .");
 
             Scanner choice1 = new Scanner(System.in);
             String problem1 = choice1.nextLine().toLowerCase();
-            if (problem1.contains("mom")) {
+            if (problem1.contains("mo")) {
                 end = true;
                 System.out.println("she answers and tell you she is on you way to pick you up  ");
+                // ememy is a teribale story no just a joke to end the game early y would you call you mom moma's boy 
             }
             if (problem1.contains("friend")) {
                 no = false;
                 System.out.println("no one answers");
             }
-            if (problem1.contains("map")) {
+            if (problem1.contains("ap")) {
 
                 System.out.println("you see a location and you start walking tword it ");
                 no = true;
             }
 
         }
+//while loop 2 #while 
 
-        while (no & !eg & !eal) {
+            while (no & !eg & !eal) {
             System.out.println("as you walk you grow hunry you can steal food , look for free food ,or beg for money ");
             Scanner choice2 = new Scanner(System.in);
             String problem2 = choice2.nextLine().toLowerCase();
-            if (problem2.contains("look")) {
+            if (problem2.contains("ok")) {
                 no = true;
                 System.out.println("you look but where unsucsessful ");
             }
-            if (problem2.contains("steal")) {
+            if (problem2.contains("ste")) {
                 no = false;
                 eal = true;
 
             }
-            if (problem2.contains("beg")) {
+            if (problem2.contains("eg")) {
                 no = false;
                 eg = true;
 
@@ -86,7 +88,10 @@ public class Renteria_7_textbasegame {
                 System.out.println("you buy food and take a taxie to the location on your phone ");
                 j = true;
             }
-            while (j & !erk & !ait & !elo) {
+        }
+    }
+     static public void j(){
+            while (!erk & !ait & !elo) {
                 System.out.println("as you arive you look and see a holel you enter and you can take the elovater to a floor, give your name too the clerk, or just wait in the loby");
                 Scanner choice3 = new Scanner(System.in);
                 String problem3 = choice3.nextLine().toLowerCase();
@@ -106,17 +111,22 @@ public class Renteria_7_textbasegame {
                 }
                 while ( ait & !j ) {
                     System.out.println("she shoves you in to the car and tells you we are going to the palace. as you arive the car blows up yuo passout ");
-                    System.out.println("you wake up it was all a dream you are still at the hotel");
+                    System.out.println("you wake up it was all a dream you are still at the hotel. you walk out side ");
                        
                             
                 }
 
             }
+     }
+     static public void end (){
+         
+             System.out.println("the end");
+         
+     }
 
-        }
+       
+       
 
-        if (end) {
-            System.out.println("the end");
-        }
-    }
-}
+        
+    
+
